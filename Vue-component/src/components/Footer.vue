5e481ccb3d01b2a0ea89fcc82a047d3d70616dae
Mahesh Kumar Kumawat <pot30270@posten.local>
2019-05-02 22:11:29 +0200

    vue-component practice

---
+++ Vue-component/src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="row">
+        <div class="col-xs-12">
+            <footer>
+                <p>footer goes here</p>
+            </footer>
+        </div>
+    </div>
+</template>
